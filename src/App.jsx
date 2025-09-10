8b9a0d252da55f653c25119960a03b231640ca6d
Shrey Vats <137366687+Shrey-Vats@users.noreply.github.com>
2025-09-10 10:08:31 +0530

    Added My Employee and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/pages/Layout";
 
 function App() {
- 
-
   return (
-    <>
+    <Router>
       <div>
         <Layout />
       </div>
-    </>
+    </Router>
   );
 }
 
