08663317b73144ffc3b0a9c7f6c056fa9b4768b5
Your Name <your@email.com>
2025-09-10 10:30:25 +0530

    noo chnages made

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import Layout from "./components/pages/Layout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
 
 function App() {
  
 
   return (
     <>
+       <Router>
       <div>
         <Layout />
       </div>
+    </Router>
+
     </>
   );
 }
